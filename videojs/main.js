d4741595341d48c6241805b4dd0615e37c3d8e20
toppogi3 <98142059+toppogi3@users.noreply.github.com>
2022-04-25 15:11:07 +0900

    Java for picture.html
    Version-4 Java for picture.html

---
+++ videojs/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+{
+  function play() {
+    setTimeout(() => {
+      images[currentIndex].classList.remove('current');
+      currentIndex++;
+      if (currentIndex > images.length -1) {
+        currentIndex = 0;
+      }
+      images[currentIndex].classList.add('current');
+      play();
+    }, 3000);
+  }
+  
+  const images = document.querySelectorAll('.hero img');
+  let currentIndex = 0;
+
+  play();
+}
